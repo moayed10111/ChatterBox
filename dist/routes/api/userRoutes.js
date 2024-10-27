305cf2d8f5f2ca969f1da898ee05140578e229b6
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-10-27 07:58:28 -0400

    MMM

--- dist/routes/api/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, deleteFriend, } from "../../controllers/userController.js";
-router.route("/").get(getAllUsers).post(createUser);
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
-router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
-export default router;
